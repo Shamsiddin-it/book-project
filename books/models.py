aabb8d08104ab4a92b2370e79d36513e1d7b1225
Shamsiddin-it <samsiddinarbobov@gmail.com>
2025-07-28 10:53:49 +0500

    added image to category

--- books/models.py
+++ books/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Category(models.Model):
     """Категория книги — жанр, тема и т.д."""
     name = models.CharField(max_length=100, unique=True)
+    image = models.ImageField(upload_to='category_images/')
 
     def __str__(self):
         return self.name
